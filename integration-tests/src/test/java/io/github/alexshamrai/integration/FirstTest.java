d590527a5cd679923ca0e0d5306f2c57d853235f
Alex Shamrai <gibsonshamray@gmail.com>
2024-11-29 22:54:29 +0200

    Switch to thread-safe list for test storage
    Replaced `ArrayList` with `CopyOnWriteArrayList` to ensure thread safety when accessing and modifying the list of tests in `CtrfExtension`. This change addresses potential concurrency issues when multiple threads interact with the test collection during execution.

---
+++ integration-tests/src/test/java/io/github/alexshamrai/integration/FirstTest.java
@@ -0,0 +1,18 @@
+package io.github.alexshamrai.integration;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTest extends BaseIntegrationTest {
+
+    @Test
+    void firstTestOne() {
+        System.out.println("firstTestOne");
+    }
+
+    @Test
+    void firstTestTwo() {
+        System.out.println("firstTestTwo");
+    }
+
+}
+
